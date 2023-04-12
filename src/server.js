d39db9a9d320fb0c3c95fce267f88bc0db41ec27
Alexc73 <127415658+Alexc73@users.noreply.github.com>
2023-04-12 15:44:25 +0100

    last update

--- src/server.js
+++ src/server.js
@@ -1,11 +1,32 @@
 require("dotenv").config();
 require("./db/connection");
 
-const express = require("express")
+const express = require("express");
+
 
 const app = express()
 
+const Book = require("./books/model");
+
 app.use(express.json());
+
+app.post("/books/addbook", async (req, res) => {
+    try {
+        const newBook = await Book.create({
+            title: req.body.title,
+            author: req.body.author,
+            genre: req.body.genre,
+        });
+        const successResponse = {
+            message: "success",
+            newBook: newBook,
+        };
+        res.status(201).json(successResponse)
+    } catch (error) {
+        console.log(error);
+        
+    }
+});
 
 app.listen(5001, () => console.log("Server is listening"));
 
