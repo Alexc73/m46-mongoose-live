78abc865319f2142355f0c221536f7b2060acf2e
Alexc73 <127415658+Alexc73@users.noreply.github.com>
2023-04-12 17:48:34 +0100

    add put and delete

--- src/server.js
+++ src/server.js
@@ -40,5 +40,46 @@ app.post("/books/addbook", async (req, res) => {
     }
 });
 
+// PUT route to update a book's author
+app.put("/books/:id/updateauthor", async (req, res) => {
+    try {
+      const updatedBook = await Book.findByIdAndUpdate(
+        req.params.id,
+        { author: req.body.author },
+        { new: true }
+      );
+      if (!updatedBook) {
+        return res.status(404).json({ message: "Book not found" });
+      }
+      const successResponse = {
+        message: "success",
+        updatedBook: updatedBook,
+      };
+      res.status(200).json(successResponse);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Server error" });
+    }
+  });
+  
+  // DELETE route to delete a book
+  app.delete("/books/:id/deletebook", async (req, res) => {
+    try {
+      const deletedBook = await Book.findByIdAndDelete(req.params.id);
+      if (!deletedBook) {
+        return res.status(404).json({ message: "Book not found" });
+      }
+      const successResponse = {
+        message: "success",
+        deletedBook: deletedBook,
+      };
+      res.status(200).json(successResponse);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Server error" });
+    }
+  });
+
+
 app.listen(5001, () => console.log("Server is listening"));
 
